8e7af5d220b2067e4026785fe4699d9160afdd60
Conrad Nguyen <conradn86@gmail.com>
2025-02-02 20:58:50 -0600

    add dvat app

---
+++ js/dvat.js
@@ -0,0 +1,170 @@
+function set_current_time(value) {
+    let current_time = document.getElementById('current_time');
+    current_time.innerHTML = value.toFixed(1);
+}
+
+function set_current_distance(value) {
+    let current_distance = document.getElementById('current_distance');
+    current_distance.innerHTML = value.toFixed(1);
+}
+
+function set_current_velocity(value) {
+    let current_velocity = document.getElementById('current_velocity');
+    current_velocity.innerHTML = value.toFixed(1);
+}
+
+function set_current_acceleration(value) {
+    let current_acceleration = document.getElementById('current_acceleration');
+    current_acceleration.innerHTML = value.toFixed(1);
+}
+
+function set_bluetooth_status(value) {
+    let current_acceleration = document.getElementById('bluetooth_status');
+    current_acceleration.innerHTML = value;
+}
+
+var rsc_characteristic;
+
+function start_bluetooth() {
+    let serviceUuid = 'running_speed_and_cadence';
+    let characteristicUuid = 'rsc_measurement';
+
+    set_bluetooth_status('Requesting Bluetooth Device...');
+    navigator.bluetooth.requestDevice({
+            filters: [{
+                services: [serviceUuid]
+            }]
+        })
+        .then(device => {
+            set_bluetooth_status('Connecting to GATT Server...');
+            return device.gatt.connect();
+        })
+        .then(server => {
+            set_bluetooth_status('Getting Service...');
+            return server.getPrimaryService(serviceUuid);
+        })
+        .then(service => {
+            set_bluetooth_status('Getting Characteristic...');
+            return service.getCharacteristic(characteristicUuid);
+        })
+        .then(characteristic => {
+            rsc_characteristic = characteristic;
+            return rsc_characteristic.startNotifications().then(_ => {
+                set_bluetooth_status('Notifications started');
+                rsc_characteristic.addEventListener('characteristicvaluechanged',
+                    handle_notifications);
+            });
+        })
+        .catch(error => {
+            set_bluetooth_status('Error! ' + error);
+        });
+}
+
+function stop_bluetooth() {
+    if (rsc_characteristic) {
+        rsc_characteristic.stopNotifications()
+            .then(_ => {
+                set_bluetooth_status('Notifications stopped');
+                rsc_characteristic.removeEventListener('characteristicvaluechanged',
+                    handle_notifications);
+            })
+            .catch(error => {
+                set_bluetooth_status('Error! ' + error);
+            });
+    }
+}
+
+function get_velocity_from_event(event) {
+    // convert from m/s to mph
+    return (event.target.value.getUint16(1, true) / 256.0 * 3600.0 / 1609.34)
+}
+
+function handle_notifications(event) {
+    let velocity = get_velocity_from_event(event);
+    let timestamp = event.timeStamp / 1000.0;
+    handle_run_data([timestamp, velocity]);
+}
+
+
+let velocity_vs_time_canvas = document.getElementById('velocity_vs_time');
+velocity_vs_time_canvas.width = 600;
+velocity_vs_time_canvas.height = 400;
+let start_time = Date.now();
+
+
+const dvat_graph_config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Velocity',
+            backgroundColor: 'rgb(74, 219, 255)',
+            borderColor: 'rgb(74, 219, 255)',
+            data: [],
+        }]
+    },
+    options: {
+        scales: {
+            x: {
+                beginAtZero: true,
+                type: 'linear',
+            }
+        }
+    }
+
+};
+
+let dvat_graph = new Chart(
+    velocity_vs_time_canvas,
+    dvat_graph_config
+);
+
+let current_run_data = [];
+let previous_run_data = [];
+let first_timestamp = 0.0;
+
+function handle_run_data(run_data) {
+    let velocity = run_data[1];
+    let timestamp = run_data[0];
+    let timedelta = timestamp - first_timestamp;
+    if (velocity > 0) {
+        if (current_run_data.length == 0) {
+            reset_chart();
+            first_timestamp = timestamp;
+            set_current_velocity(velocity);
+            set_current_time(0.0);
+            add_data(dvat_graph, 0.0, velocity);
+        }
+        let timedelta = timestamp - first_timestamp;
+        if (timedelta >= 0.1) {
+            set_current_velocity(velocity);
+            set_current_time(timedelta);
+            add_data(dvat_graph, timedelta, velocity);
+        }
+        current_run_data.push([timestamp, velocity]);
+    } else {
+        if (current_run_data.length > 0) {
+            current_run_data = [];
+        }
+    }
+}
+
+function reset_chart() {
+    dvat_graph.data.labels = [];
+    dvat_graph.data.datasets.forEach((dataset) => {
+        dataset.data = [];
+    });
+    dvat_graph.destroy();
+    dvat_graph = new Chart(
+        velocity_vs_time_canvas,
+        dvat_graph_config
+    );
+}
+
+function add_data(chart, timestamp, velocity) {
+    chart.data.labels.push(timestamp);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(velocity);
+    });
+    chart.update();
+}
---
+++ js/flashcards.js
@@ -0,0 +1,221 @@
+const default_config = {
+    "stroke_order_font": false,
+    "prompt": "japanese",
+};
+const default_card = {
+    "japanese": "完全",
+    "english": "complete",
+    "furigana": "完[か]全[ぜん]",
+    "kana": "かんぜん",
+    "answer": "XXXXX"
+}
+var current_answer_id = "answer";
+var deck_name, deck_size, deck_offset;
+var current_answer_value = default_card["answer"];
+var current_card_index = 0;
+var max_card_index = 0;
+var card_set = [];
+var card_info = {
+    "show-audio": false,
+    "show-japanese": true,
+    "show-furigana": false,
+    "show-english": false,
+    "show-kana": false,
+}
+var focus_input = true;
+
+function get_deck_options_hash() {
+    [deck_name, deck_size, deck_offset] = window.location.hash.replace('#', '').split(',');
+    let deck_select = document.getElementById("deck-select");
+    deck_select.value = deck_name;
+    let deck_size_select = document.getElementById("deck-size-select");
+    deck_size_select.value = deck_size;
+    let deck_offset_input = document.getElementById("deck-offset");
+    deck_offset_input.value = deck_offset;
+}
+
+function get_randomize(argument) {
+    let randomize = document.getElementById("randomize");
+    return randomize.checked
+}
+
+function set_progress(progress_value) {
+    let progress = document.getElementById('progress');
+    progress.innerHTML = progress_value;
+}
+
+function set_deck_options_hash() {
+    let deck_select = document.getElementById("deck-select");
+    deck_name = deck_select.options[deck_select.selectedIndex].value;
+    let deck_size_select = document.getElementById("deck-size-select");
+    deck_size = parseInt(deck_size_select.options[deck_size_select.selectedIndex].value);
+    let deck_offset_input = document.getElementById("deck-offset");
+    deck_offset = parseInt(deck_offset_input.value);
+    window.location.hash = [deck_name, deck_size, deck_offset].join(',')
+}
+
+function load_deck_options() {
+    [deck_name, deck_size, deck_offset] = window.location.hash.replace('#', '').split(',');
+    deck_size = parseInt(deck_size);
+    deck_offset = parseInt(deck_offset);
+    if (deck_name in decks) {
+        set_cards(deck_name, deck_size, deck_offset);
+    }
+}
+
+function toggle_card_info() {
+    for (const [key, value] of Object.entries(card_info)) {
+        let hint = document.getElementById(key);
+        card_info[key] = hint.checked;
+    }
+    reset_card_info();
+}
+
+function reset_card_info() {
+    for (const [key, value] of Object.entries(card_info)) {
+        if (value) {
+            show_card_info(key);
+        } else {
+            hide_card_info(key);
+        }
+    }
+
+}
+
+function hide_card_info(card_info_id) {
+    let hint = document.getElementById(card_info_id.replace('show-', 'card-'));
+    hint.style.display = "none";
+}
+
+function show_card_info(card_info_id) {
+    let hint = document.getElementById(card_info_id.replace('show-', 'card-'));
+    hint.style.display = "block";
+
+}
+
+function temporarily_show_card_info() {
+    for (const [key, value] of Object.entries(card_info)) {
+        let hint = document.getElementById(key.replace('show-', 'card-'));
+        if (hint.style.display === "none") {
+            hint.style.display = "block";
+        }
+    }
+}
+
+function answer_id() {
+    return "answer-" + Date.now().toString()
+}
+
+function set_japanese_font(stroke_order_font) {
+    let card_furigana = document.getElementById('card-furigana');
+    if (stroke_order_font) {
+        card_furigana.style = 'font-family:"kanji"; font-size:100px; padding:10px; margin:10px;';
+    } else {
+        card_furigana.style = '';
+    }
+
+}
+
+function set_cards(deck_name, deck_size, deck_offset) {
+    let deck = decks[deck_name];
+    set_japanese_font(deck['config']['stroke_order_font']);
+    for (const [key, value] of Object.entries(card_info)) {
+        if (key.replace('show-', '') === deck['config']['prompt']) {
+            card_info[key] = true;
+            let hint = document.getElementById(key);
+            hint.checked = true;
+        } else {
+            // card_info[key] = false;
+            // let hint = document.getElementById(key);
+            // hint.checked = false;
+        }
+    }
+    focus_input = deck['config']['focus_input'];
+    card_set = deck['cards'].slice(deck_offset - 1, deck_offset - 1 + deck_size);
+    if (get_randomize()) {
+        card_set = shuffle(card_set);
+    }
+    max_card_index = card_set.length - 1;
+    current_card_index = 0;
+    set_progress(current_card_index + 1);
+
+    set_card_text(card_set[0]);
+    set_answer(card_set[0]["answer"]);
+}
+
+function set_card_text(card) {
+    let card_japanese = document.getElementById("card-japanese");
+    card_japanese.innerHTML = card["japanese"];
+    let card_furigana = document.getElementById("card-furigana");
+    card_furigana.innerHTML = card["furigana"];
+    let card_english = document.getElementById("card-english");
+    card_english.innerHTML = card["english"];
+    let card_kana = document.getElementById("card-kana");
+    card_kana.innerHTML = card["kana"];
+    if ("audio" in card) {
+        let card_audio_player = document.getElementById("card-audio-player");
+        let card_audio_source = document.getElementById("card-audio-source");
+        card_audio_source.src = card["audio"];
+        card_audio_player.load();
+        card_audio_player.play();
+    }
+    reset_card_info();
+}
+
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+    return array
+}
+
+function set_answer(answer_text) {
+    let answer_container = document.getElementById("answer-container");
+    let current_answer = document.getElementById(current_answer_id);
+    let new_answer_id = answer_id();
+    let new_answer = document.createElement("input");
+    let fake_input = document.createElement("input");
+    new_answer.type = "text";
+    new_answer.setAttribute("id", new_answer_id);
+    new_answer.setAttribute("onkeyup", "check_answer();");
+    answer_container.removeChild(current_answer);
+    answer_container.appendChild(new_answer);
+    current_answer_id = new_answer_id;
+    current_answer_value = answer_text;
+    if (focus_input) {
+        // Hack to get iOS to focus on new input.
+        fake_input.setAttribute('type', 'text');
+        fake_input.style.position = 'absolute';
+        fake_input.style.opacity = 0;
+        fake_input.style.height = 0;
+        document.body.prepend(fake_input);
+        fake_input.focus();
+        setTimeout(() => {
+            new_answer.focus()
+            fake_input.remove()
+        }, 100)
+    }
+}
+
+function check_answer() {
+    let answer = document.getElementById(current_answer_id);
+    if (current_answer_value === answer.value) {
+        next_question()
+    }
+}
+
+function next_question() {
+    current_card_index += 1;
+    set_progress(current_card_index + 1);
+    if (current_card_index <= max_card_index) {
+        set_card_text(card_set[current_card_index]);
+        set_answer(card_set[current_card_index]["answer"]);
+    } else {
+        set_card_text(default_card);
+        set_answer(default_card["answer"]);
+    }
+}
+
+function init_flashcards(argument) {
+    get_deck_options_hash();
+    set_deck_options_hash();
+    load_deck_options();
+}
