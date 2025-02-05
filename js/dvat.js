02e2a6b5e428808dbcc3eb091f6bf52c67436a39
Conrad Nguyen <conradn86@gmail.com>
2025-02-04 21:19:46 -0600

    add max measurements.

--- js/dvat.js
+++ js/dvat.js
@@ -1,9 +1,13 @@
-function set_timer() {
-    let deck_size_select = document.getElementById("dvat-timer-select");
-    duration = parseInt(deck_size_select.value);
-    setTimeout(() => {
-        play_tone();
-    }, duration * 1000);
+function set_timer(duration) {
+    let timer_interval = setInterval(() => {
+            current_time += 100;
+            if (current_time >= duration) {
+                clearInterval(timer_interval);
+                // play_tone();
+            }
+            set_current_time(current_time);
+        },
+        100)
 }
 
 function play_tone() {
@@ -18,32 +22,37 @@ function set_current_run_log(value) {
 
 function set_current_time(value) {
     let current_time = document.getElementById('current_time');
-    current_time.innerHTML = value.toFixed(1);
+    current_time.innerHTML = (value / 1000.0).toFixed(1);
 }
 
 function set_current_distance(value) {
     let current_distance = document.getElementById('current_distance');
-    current_distance.innerHTML = value.toFixed(1);
+    current_distance.innerHTML = value.toFixed(1) + ' (' + (value * .95).toFixed(1) + ')';
 }
 
 function set_max_velocity(value) {
     let max_velocity = document.getElementById('max_velocity');
-    max_velocity.innerHTML = value.toFixed(1);
+    max_velocity.innerHTML = value.toFixed(2) + ' (' + (value * .95).toFixed(2) + ')';
 }
 
 function set_current_velocity(value) {
     let current_velocity = document.getElementById('current_velocity');
-    current_velocity.innerHTML = value.toFixed(1);
+    current_velocity.innerHTML = value.toFixed(2);
 }
 
-function set_current_acceleration(value) {
-    let current_acceleration = document.getElementById('current_acceleration');
-    current_acceleration.innerHTML = value.toFixed(1);
+function set_max_velocity_time(value) {
+    let max_velocity_time = document.getElementById('max_velocity_time');
+    max_velocity_time.innerHTML = (value / 1000.0).toFixed(1);
+}
+
+function set_max_acceleration(value) {
+    let max_acceleration = document.getElementById('max_acceleration');
+    max_acceleration.innerHTML = value.toFixed(2) + ' (' + (value * .95).toFixed(2) + ')';
 }
 
 function set_bluetooth_status(value) {
-    let current_acceleration = document.getElementById('bluetooth_status');
-    current_acceleration.innerHTML = value;
+    let bluetooth_status = document.getElementById('bluetooth_status');
+    bluetooth_status.innerHTML = value;
 }
 
 var rsc_characteristic;
@@ -102,13 +111,6 @@ function get_velocity_from_event(event) {
     return (event.target.value.getUint16(1, true) / 256.0 * 3600.0 / 1609.34)
 }
 
-function handle_notifications(event) {
-    let velocity = get_velocity_from_event(event);
-    let timestamp = event.timeStamp / 1000.0;
-    handle_run_data([timestamp, velocity]);
-}
-
-
 let velocity_vs_time_canvas = document.getElementById('velocity_vs_time');
 velocity_vs_time_canvas.width = 600;
 velocity_vs_time_canvas.height = 400;
@@ -145,33 +147,62 @@ let current_run_data = [];
 let previous_run_data = [];
 let first_timestamp = 0.0;
 let last_timestamp = 0.0;
+let last_velocity = 0.0;
 let max_velocity = 0.0;
+let current_time = 0.0; // milliseconds
+let current_distance = 0.0; // yards
+let max_acceleration = 0.0; // mph/s
+let time_at_velocity_lookup = {};
+
+function handle_notifications(event) {
+    let timestamp = event.timeStamp;
+    let velocity = get_velocity_from_event(event);
+    handle_run_data([timestamp, velocity]);
+}
+
 
 function handle_run_data(run_data) {
-    let velocity = run_data[1];
     let timestamp = run_data[0];
+    let velocity = run_data[1];
+    let deck_size_select = document.getElementById("dvat-timer-select");
+    duration = parseInt(deck_size_select.value) * 1000;
+
     if (velocity > 0) {
+        // Run is starting.
         if (current_run_data.length == 0) {
             reset_chart();
+            current_time = 0.0;
+            current_distance = 0.0;
+            last_velocity = 0.0;
             max_velocity = 0.0;
-            first_timestamp = timestamp;
-            last_timestamp = timestamp;
-            current_run_data.push([timestamp, velocity]);
-            set_current_velocity(velocity);
-            set_current_time(0.0);
-            add_data(dvat_graph, 0.0, velocity);
-            set_timer()
+            time_at_velocity_lookup = {};
+            first_timestamp = timestamp - 1000.0 / 3.7;
+            last_timestamp = timestamp - 1000.0 / 3.7;
+            current_run_data.push([0, 0.0]);
+            set_current_velocity(0.0);
+            set_timer(duration);
         }
-        if ((timestamp - last_timestamp) >= 0.1) {
+        // We only want valid data below sampling rate
+        let timestamp_diff = timestamp - last_timestamp;
+        let valid_timestamp = ((timestamp_diff) >= 100)
+        let timestamp_within_rep = ((timestamp - first_timestamp) <= duration)
+        if (valid_timestamp & timestamp_within_rep) {
             set_current_velocity(velocity);
-            set_current_time(timestamp - first_timestamp);
             add_data(dvat_graph, timestamp - first_timestamp, velocity);
             current_run_data.push([timestamp, velocity]);
+            current_distance += timestamp_diff / 1000 * velocity * 3600 / 1706;
+            set_current_distance(current_distance);
+            max_acceleration = Math.max(max_acceleration, (velocity - last_velocity) / (timestamp_diff / 1000))
+            set_max_acceleration(max_acceleration);
             last_timestamp = timestamp;
+            last_velocity = velocity;
+            time_at_velocity_lookup[velocity] = (timestamp - first_timestamp);
+            max_velocity = Math.max(velocity, max_velocity);
+            set_max_velocity(max_velocity);
+            set_max_velocity_time(time_at_velocity_lookup[max_velocity]);
         }
-        max_velocity = Math.max(velocity, max_velocity);
-        set_max_velocity(max_velocity);
     } else {
+        // Reset data if exists. No current rep going on.
         if (current_run_data.length > 0) {
             current_run_data = [];
         }
@@ -191,7 +222,8 @@ function reset_chart() {
 }
 
 function add_data(chart, timestamp, velocity) {
-    chart.data.labels.push(timestamp);
+    // timestamp in milliseconds. rescale to make reasonable.
+    chart.data.labels.push(timestamp / 1000.0);
     chart.data.datasets.forEach((dataset) => {
         dataset.data.push(velocity);
     });
